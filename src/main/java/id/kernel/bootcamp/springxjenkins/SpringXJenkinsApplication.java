668f649ec9daae51c0b048f3a46f9c968708db43
Kesit Budi Kusumo <kesit@kernel.id>
2023-12-10 11:31:51 +0700

    initial commit

---
+++ src/main/java/id/kernel/bootcamp/springxjenkins/SpringXJenkinsApplication.java
@@ -0,0 +1,13 @@
+package id.kernel.bootcamp.springxjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/kernel/bootcamp/springxjenkins/SpringXJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.kernel.bootcamp.springxjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringXJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
